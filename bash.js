885d50aa91c86ae8e2f315e45c81ce03f368fc48
Laura <laura.waters22@gmail.com>
2018-11-06 12:58:22 -0500

    pwd implemented in pash.js

---
+++ bash.js
@@ -0,0 +1,13 @@
+process.stdout.write('prompt > ');
+
+
+process.stdin.on('data', (data) => {
+  const cmd = data.toString().trim();
+
+  if (cmd === 'pwd') {
+    process.stdout.write(__dirname);
+  } else {
+    process.stdout.write('you typed ' + cmd);
+  }
+  process.stdout.write('\nprompt > ');
+});
